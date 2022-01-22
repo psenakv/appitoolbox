2ea9b939fa2ceeaea27f2540336e439791f9ca5f
Vojtěch Pšenák <me@vojtapsenak.cz>
2022-01-22 02:46:02 +0100

    json logging

--- bin/appitoolbox.dart
+++ bin/appitoolbox.dart
@@ -16,20 +16,49 @@ Response _rootHandler(Request req) {
   return Response.notFound("Page not found.");
 }
 
-void _jsonLogger(String message, bool error) {
-  Map<String, dynamic> log = {
-    'message': message,
-  };
-  if (error) log['severity'] = "ERROR";
-  print(jsonEncode(log));
-}
+Middleware _jsonLogger({void Function(String message, bool isError)? logger}) =>
+    (innerHandler) {
+      return (request) {
+        var startTime = DateTime.now();
+        var watch = Stopwatch()..start();
+
+        return Future.sync(() => innerHandler(request)).then((response) {
+          Map<String, dynamic> log = {
+            'severity': 'INFO',
+            'time': startTime.toString(),
+            'httpRequest': {
+              'requestMethod': request.method,
+              'requestUrl': request.requestedUri.toString(),
+              'status': response.statusCode,
+              'latency': (watch.elapsed.inMicroseconds / 1000000).toString(),
+            },
+          };
+          print(jsonEncode(log));
+
+          return response;
+        }, onError: (Object error, StackTrace stackTrace) {
+          if (error is HijackException) throw error;
+
+          Map<String, dynamic> log = {
+            'severity': 'ERROR',
+            'time': startTime.toString(),
+            'httpRequest': {
+              'requestMethod': request.method,
+              'requestUrl': request.requestedUri.toString(),
+              'latency': (watch.elapsed.inMicroseconds / 1000000).toString(),
+            },
+          };
+          print(jsonEncode(log));
+
+          throw error;
+        });
+      };
+    };
 
 void main(List<String> args) async {
   final ip = InternetAddress.anyIPv4;
 
-  final _handler = Pipeline()
-      .addMiddleware(logRequests(logger: _jsonLogger))
-      .addHandler(_router);
+  final _handler = Pipeline().addMiddleware(_jsonLogger()).addHandler(_router);
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
   final server = await serve(_handler, ip, port);
   dotenv.load();
